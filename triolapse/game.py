4b9a5fd4fbbc328338c38c840ae9cc268ebfe1e7
Adam Vajda <vjdad4m@gmail.com>
2023-09-01 17:17:52 +0200

    greedy search

--- triolapse/game.py
+++ triolapse/game.py
@@ -1,4 +1,5 @@
 from collections import namedtuple
+import threading
 import random
 
 import numpy as np
@@ -72,24 +73,57 @@ def draw_grid(grid):
         row_str = row_str.replace("1", "⬛")
         print(row_str)
 
+def generate_greedy_solution(N: int) -> (int, np.ndarray):
+    game = NoThreeInLine(N)
+    column_counters = [0] * N
+    moves = list(range(N))
+    
+    for row in range(N):
+        for _ in range(2):
+            move = random.choice(moves)
+            column_counters[move] += 1
+            if column_counters[move] == 2:
+                moves.remove(move)
+            game.make_move(move + row * N)
+
+    n_placed, _ = game.calculate_reward()
+    return n_placed, game.states[-1]
+
+def greedy_solution_wrapper(N, callback, _id):
+    while True:
+        n_placed, state = generate_greedy_solution(N)
+        callback((n_placed, state, _id))
+
 def main():
     N = int(input('Grid size: '))
-    best_reward = -1
-    while True:
-        game = NoThreeInLine(N)
-        while not game.is_terminal():
-            legal_moves = game.get_legal_moves()
-            legal_move_idxs = [i for i in range(len(legal_moves)) if legal_moves[i]]
-            move = random.choice(legal_move_idxs)
-            game.make_move(move)
-        
-        n_placed, reward = game.calculate_reward()
-        if reward > best_reward:
-            best_reward = reward
-            print('--', n_placed, '--')
-            draw_grid(game.states[n_placed])
-            if best_reward == 1:
-                break
+    highest_n_placed = -1
+    lock = threading.Lock()
+    should_stop = False
+
+    def result_callback(result):
+        nonlocal highest_n_placed, should_stop
+        n_placed, state, _id = result
+        with lock:
+            if n_placed > highest_n_placed:
+                highest_n_placed = n_placed
+                text_length = (N + len(str(n_placed))) // 2
+                print('-' * text_length, n_placed, '-' * text_length, 'from:', _id)
+                draw_grid(state)
+                if highest_n_placed == 2 * N:
+                    should_stop = True
+                    return True
+            return False
+
+    threads = []
+
+    for i in range(32):
+        thread = threading.Thread(target=greedy_solution_wrapper, args=(N, result_callback, i + 1), daemon=True)
+        threads.append(thread)
+        thread.start()
+        print('starting thread', i+1)
+
+    while not should_stop:
+        pass
 
 if __name__ == '__main__':
     main()
